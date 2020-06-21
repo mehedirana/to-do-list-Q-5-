db95e2b386c7384a4547ca3354d1034b3a28e99e
mehedi rana <mdmehedirana@gmail.com>
2020-06-21 13:14:29 +0600

    add props

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <Home/>
+    <Home data= "my first program on vue"/>
   </div>
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
         <h1>hello i am from home</h1>
+        <h1>{{data}}</h1>
  
    </div>
 </template>
  <script>
 export default {
-  name: 'Home'
-}
+  name: 'Home',
+  props:{
+      data: String 
+  }
+};
 </script>
