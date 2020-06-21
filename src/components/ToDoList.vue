30df314a5a1e2951e665b882773808fbb4943280
mehedi rana <mdmehedirana@gmail.com>
2020-06-21 21:07:13 +0600

    add remove item

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,9 +1,15 @@
 <template>
     <div>
-      <input type="text" class="todoInput" v-model="newTodo" @keyup.enter="addTodo">
+      <input type="text" class="todo-input" v-model="newTodo" @keyup.enter="addTodo" >
       <div v-for="todo in todos" :key="todo.id" class="todo-item">
-        {{todo.title}}
-      </div>
+          <div>
+              {{todo.title}}
+          </div>
+
+         <div class="remove-item">
+           &times;
+         </div>
+      </div>  
     </div>
 </template>
 
@@ -13,6 +19,7 @@ export default {
     data(){
         return{
             newTodo: '',
+            idFortodo: 3,
             todos: [
                 {
                     'id': 1,
@@ -29,12 +36,36 @@ export default {
     },
     methods:{
         addTodo(){
-            alert("adding")
+            if(this.newTodo.trim().length==0) return
+            this.todos.push({
+               id: this.idFortodo,
+               title: this.newTodo,
+               completed: false, 
+            })
+            this.newTodo= '',
+            this.idFortodo++
         }
     }
 }
 </script>
 
-<style lan="scss" scoped>
+<style lang="scss">
+.todo-input {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 100px;
+    font-size: 15px;
+    /* margin-top: 200px; */
+    &:focus{
+        outline: 0;
+    }
+}
+.todo-item {
+        margin-bottom: 15px;
+        display:  flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
 
 </style>
