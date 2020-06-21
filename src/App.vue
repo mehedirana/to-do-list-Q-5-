7b705deda2171f4a2c56c9e598feb6731921a0eb
mehedi rana <mdmehedirana@gmail.com>
2020-06-21 14:08:29 +0600

    add loop n td

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Home/>
+     <!--<HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home/> -->
     <Home data= "my first program on vue"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+//import HelloWorld from './components/HelloWorld.vue';
 import Home from './components/Home.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    //HelloWorld,
     Home
     
   }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,20 @@
     <div>
         <h1>hello i am from home</h1>
         <h1>{{data}}</h1>
+        <button v-on:mouseover="handle('welcome')">click me</button>
+        <table>
+           <tr>
+             <td>id </td>
+             <td>name </td>
+             <td>email </td>
+           </tr>
+
+           <tr v-for="user in arr" :key="user.id">
+             <td>{{user.id}}</td>
+             <td>{{user.name}}</td>
+             <td>{{user.email}} </td>
+           </tr>
+        </table>
  
    </div>
 </template>
@@ -10,6 +24,20 @@ export default {
   name: 'Home',
   props:{
       data: String 
+  },methods: {
+      handle(data){
+          console.log("called")
+          alert(data)
+      }
+  },data(){
+      return {
+          arr:[
+              {id:1, name:"mehedi", email:"mehedi@gmai.com"},
+              {id:2, name:"rajib", email:"rajib@gmai.com"},
+              {id:3, name:"imran", email:"imran@gmai.com"},
+              {id:4, name:"mashuk", email:"mashuk@gmai.com"},
+          ]
+      }
   }
 };
 </script>
